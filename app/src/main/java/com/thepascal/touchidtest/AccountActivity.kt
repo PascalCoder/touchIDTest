7b1aaf8e096dcfda8a296b1dcbd6d015472e1ad5
PascalCoder <pbaconqueror7@yahoo.com>
2019-08-22 17:06:06 -0400

    add a new activity to redirect user after sign in

---
+++ app/src/main/java/com/thepascal/touchidtest/AccountActivity.kt
@@ -0,0 +1,12 @@
+package com.thepascal.touchidtest
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AccountActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_account)
+    }
+}
